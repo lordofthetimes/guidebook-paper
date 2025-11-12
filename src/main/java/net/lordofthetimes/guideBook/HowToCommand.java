3045d6e7885ed2ecd27ad6e73ab5891b727180bc
LordofTheTime <a@a.a>
2025-11-12 21:13:00 +0100

    Added support for hover, multiple pages and simplified the config file. Added chat message in case the config file is missing a book or has no pages

--- src/main/java/net/lordofthetimes/guideBook/HowToCommand.java
+++ src/main/java/net/lordofthetimes/guideBook/HowToCommand.java
@@ -2,14 +2,14 @@ package net.lordofthetimes.guideBook;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
@@ -32,7 +32,7 @@ public class HowToCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        if(commandSender instanceof Player player){
+        if (commandSender instanceof Player player) {
             if (!player.hasPermission("guidebook.open")) {
                 player.sendMessage("You don't have permission to use this command!");
                 return true;
@@ -46,46 +46,61 @@ public class HowToCommand implements CommandExecutor {
         return false;
     }
 
-    private void openBook(Player player){
+    private void openBook(Player player) {
         ItemStack bookItem = new ItemStack(Material.WRITTEN_BOOK);
         BookMeta meta = (BookMeta) bookItem.getItemMeta();
 
-        meta.setTitle("HowTo");
-        meta.setAuthor("Server");
+        meta.setTitle("GuideBook");
+        meta.setAuthor("GuideBook");
 
 
-        List<?> content = this.plugin.getConfig().getList("howto.content");
-        Component bookContent;
-        List<Component> parts = new ArrayList<>();
-        if(content != null){
-            for(Object obj : content){
-                Component part = null;
-                if(obj instanceof String text){
-                    parts.add(MiniMessage.miniMessage().deserialize(text));
-                } else if(obj instanceof Map<?, ?> map){
-                    if (map.containsKey("link")) {
-                        Object maybeLink = map.get("link");
-                        if (maybeLink instanceof Map<?, ?> link) {
-                            if (link.containsKey("text") && link.containsKey("url")) {
-                                parts.add(
-                                        MiniMessage.miniMessage().deserialize(link.get("text").toString())
-                                                .clickEvent(ClickEvent.openUrl(link.get("url").toString()))
-                                );
-                            }
+        ConfigurationSection book = this.plugin.getConfig().getConfigurationSection("book");
+
+        if (book == null) {
+            // Book doesn't exist at all
+            player.sendMessage(
+                    MiniMessage.miniMessage().deserialize("<red><bold>[GuideBook] The book has not beed configured!</bold></red>"));
+        } else if (book.getKeys(false).isEmpty()) {
+            // Book exists but has no pages
+            player.sendMessage(
+                    MiniMessage.miniMessage().deserialize("<red><bold>[GuideBook] The book has no pages!</bold></red>"));
+        }else{
+            //loops over every page
+            for(String page : book.getKeys(false)){
+
+                List<?> content = book.getList(page);
+                Component bookContent;
+                List<Component> parts = new ArrayList<>();
+
+                //if page has content set
+                if (content != null) {
+                    for (Object obj : content) {
+                        if (obj instanceof String text) {
+                            parts.add(MiniMessage.miniMessage().deserialize(text));
+                        } else if (obj instanceof Map<?, ?> map && map.containsKey("text")) {
+
+                            Component part = MiniMessage.miniMessage().deserialize(map.get("text").toString());
+
+                            if (map.containsKey("url"))
+                                part = part.clickEvent(ClickEvent.openUrl(map.get("url").toString()));
+
+                            if (map.containsKey("hover"))
+                                part = part.hoverEvent(HoverEvent.showText(MiniMessage.miniMessage().deserialize(map.get("hover").toString())));
+
+                            parts.add(part);
+
                         }
                     }
+                    bookContent = Component.join(Component.newline(), parts);
+                    meta.addPages(bookContent);
                 }
             }
-            bookContent = Component.join(Component.newline(), parts);
+
+            bookItem.setItemMeta(meta);
+
+            player.openBook(bookItem);
         }
-        else{
-            bookContent = Component.text("The book content was not configured");
-        }
 
-        meta.addPages(bookContent);
 
-        bookItem.setItemMeta(meta);
-
-        player.openBook(bookItem);
     }
 }
